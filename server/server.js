4308e30fa340538242db28a8c8960c40fae676a5
js22 <vivek4329@gmail.com>
2018-08-31 11:39:49 +0530

    GET /User/ID

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-
+const {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
@@ -39,8 +39,24 @@ app.get('/user',(req,res) => {
     res.send({doc})
   }, (e) => {
     res.status(400).send(e)
-  })
-})
+  });
+});
+
+app.get('/user/:id', (req,res) => {
+  // res.send(req.params);
+  var id = req.params.id;
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+  User.findById(id).then((user) => {
+    if(!user){
+      return res.status(404).send();
+    }
+    res.send({user})
+  }, (e) => {
+    res.status(400).send(e)
+  });
+});
 
 app.listen(3000, () => {
   console.log('Started on port 3000');
